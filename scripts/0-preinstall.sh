fd5cbc1f0fc3ef30bcea1df5ae7a0ed3daa27a83
ZYR0Z <contact@zyroz.dev>
2024-05-31 16:23:18 +0200

    FEAT: added i3wm config

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -10,7 +10,7 @@
 #github-action genshdoc
 #
 # @file Preinstall
-# @brief Contains the steps necessary to configure and pacstrap the install to selected drive. 
+# @brief Contains the steps necessary to configure and pacstrap the install to selected drive.
 echo -ne "
 -------------------------------------------------------------------------
    █████╗ ██████╗  ██████╗██╗  ██╗████████╗██╗████████╗██╗   ██╗███████╗
@@ -54,14 +54,14 @@ echo -ne "
 "
 umount -A --recursive /mnt # make sure everything is unmounted before we start
 # disk prep
-sgdisk -Z ${DISK} # zap all on disk
+sgdisk -Z ${DISK}         # zap all on disk
 sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
-sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
+sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK}  # partition 1 (BIOS Boot Partition)
 sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
-if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
+sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK}       # partition 3 (Root), default start, remaining
+if [[ ! -d "/sys/firmware/efi" ]]; then                                # Checking for bios system
     sgdisk -A 1:set:2 ${DISK}
 fi
 partprobe ${DISK} # reread partition table to ensure it is correct
@@ -72,8 +72,8 @@ echo -ne "
                     Creating Filesystems
 -------------------------------------------------------------------------
 "
-# @description Creates the btrfs subvolumes. 
-createsubvolumes () {
+# @description Creates the btrfs subvolumes.
+createsubvolumes() {
     btrfs subvolume create /mnt/@
     btrfs subvolume create /mnt/@home
     btrfs subvolume create /mnt/@var
@@ -82,24 +82,24 @@ createsubvolumes () {
 }
 
 # @description Mount all btrfs subvolumes after root has been mounted.
-mountallsubvol () {
+mountallsubvol() {
     mount -o ${MOUNT_OPTIONS},subvol=@home ${partition3} /mnt/home
     mount -o ${MOUNT_OPTIONS},subvol=@tmp ${partition3} /mnt/tmp
     mount -o ${MOUNT_OPTIONS},subvol=@var ${partition3} /mnt/var
     mount -o ${MOUNT_OPTIONS},subvol=@.snapshots ${partition3} /mnt/.snapshots
 }
 
-# @description BTRFS subvolulme creation and mounting. 
-subvolumesetup () {
-# create nonroot subvolumes
-    createsubvolumes     
-# unmount root to remount with subvolume 
+# @description BTRFS subvolulme creation and mounting.
+subvolumesetup() {
+    # create nonroot subvolumes
+    createsubvolumes
+    # unmount root to remount with subvolume
     umount /mnt
-# mount @ subvolume
+    # mount @ subvolume
     mount -o ${MOUNT_OPTIONS},subvol=@ ${partition3} /mnt
-# make directories home, .snapshots, var, tmp
+    # make directories home, .snapshots, var, tmp
     mkdir -p /mnt/{home,var,tmp,.snapshots}
-# mount subvolumes
+    # mount subvolumes
     mountallsubvol
 }
 
@@ -122,17 +122,17 @@ elif [[ "${FS}" == "ext4" ]]; then
     mount -t ext4 ${partition3} /mnt
 elif [[ "${FS}" == "luks" ]]; then
     mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
-# enter luks password to cryptsetup and format root partition
+    # enter luks password to cryptsetup and format root partition
     echo -n "${LUKS_PASSWORD}" | cryptsetup -y -v luksFormat ${partition3} -
-# open luks container and ROOT will be place holder 
+    # open luks container and ROOT will be place holder
     echo -n "${LUKS_PASSWORD}" | cryptsetup open ${partition3} ROOT -
-# now format that container
+    # now format that container
     mkfs.btrfs -L ROOT ${partition3}
-# create subvolumes for btrfs
+    # create subvolumes for btrfs
     mount -t btrfs ${partition3} /mnt
     subvolumesetup
-# store uuid of encrypted partition for grub
-    echo ENCRYPTED_PARTITION_UUID=$(blkid -s UUID -o value ${partition3}) >> $CONFIGS_DIR/setup.conf
+    # store uuid of encrypted partition for grub
+    echo ENCRYPTED_PARTITION_UUID=$(blkid -s UUID -o value ${partition3}) >>$CONFIGS_DIR/setup.conf
 fi
 
 # mount target
@@ -152,11 +152,11 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
-echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
+echo "keyserver hkp://keyserver.ubuntu.com" >>/mnt/etc/pacman.d/gnupg/gpg.conf
 cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
-genfstab -L /mnt >> /mnt/etc/fstab
+genfstab -L /mnt >>/mnt/etc/fstab
 echo " 
   Generated /etc/fstab:
 "
@@ -177,9 +177,9 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
-if [[  $TOTAL_MEM -lt 8000000 ]]; then
+if [[ $TOTAL_MEM -lt 8000000 ]]; then
     # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
-    mkdir -p /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
+    mkdir -p /mnt/opt/swap  # make a dir that we can apply NOCOW to to make it btrfs-friendly.
     chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
     dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
     chmod 600 /mnt/opt/swap/swapfile # set permissions.
@@ -187,7 +187,7 @@ if [[  $TOTAL_MEM -lt 8000000 ]]; then
     mkswap /mnt/opt/swap/swapfile
     swapon /mnt/opt/swap/swapfile
     # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
-    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
+    echo "/opt/swap/swapfile	none	swap	sw	0	0" >>/mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
 fi
 echo -ne "
 -------------------------------------------------------------------------
