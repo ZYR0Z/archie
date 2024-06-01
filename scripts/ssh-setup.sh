da06b6ba195a8b9817163e74acdb157717517700
zyr0z <contact@zyroz.dev>
2024-06-01 17:54:21 +0200

    forgot export before ENV :(

--- scripts/ssh-setup.sh
+++ scripts/ssh-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GH_TOKEN="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"
+export GH_TOKEN="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"
 
 primary_email=$(gh api "https://api.github.com/user/emails" -q "'.[] | select(.primary) | .email'")
 
