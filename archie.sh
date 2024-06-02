a0bb9e7c0740848f65450a77ae476a90855b4201
ZYR0Z <contact@zyroz.dev>
2024-06-02 11:32:10 +0200

    minor tui change

--- archie.sh
+++ archie.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#github-action genshdoc
-#
-# @file archie
-# @brief Entrance script that launches children scripts for each phase of installation.
-
 # Find the name of the folder the scripts are in
 set -a
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
@@ -21,7 +16,6 @@ echo -ne "
 -------------------------------------------------------------------------
                     Automated Arch Linux Installer
 -------------------------------------------------------------------------
-                Scripts are in directory named archie
 "
 (bash $SCRIPT_DIR/scripts/startup.sh) |& tee startup.log
 source $CONFIGS_DIR/setup.conf
